273bfd7399d1a290521fca7fc19f96f89ef22e4d
Eric Larson <LarsonEric@Live.com>
2017-11-17 23:57:30 -0600

    Added User Resource Security

--- src/app/components/testing/testing.component.ts
+++ src/app/components/testing/testing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AngularFirestore } from "angularfire2/firestore";
 import { User } from "../../services/auth-service/auth.service";
+import { Budget } from "../../services/database/database.service";
 
 @Component({
   selector: 'app-testing',
@@ -13,21 +14,23 @@ export class TestingComponent implements OnInit {
   constructor(private afs: AngularFirestore) { }
 
   ngOnInit() {
-      const user: any = {
+      const data: Budget = {
           userId: 'Ayq6YA5pS1XXJtKfSLJNpvanzSj1',
-          email: 'asdf',
+          budgetName: 'b',
+          currencyType: 'usd'
       };
 
-      this.afs.doc('users/Ayq6YA5pS1XXJtKfSLJNpvanzSj1').set(user);
+      this.afs.collection('budgets').add(data);
 
-      // this.afs.doc('users/Ayq6YA5pS1XXJtKfSLJNpvanzSj1').valueChanges().subscribe(x => {
+      //Read - get test
+      // this.afs.doc('/budgets/IHbuKGDQRFnM0YAm8znX').valueChanges().subscribe(x => {
       //     console.log(x);
-      // })
-      //
-      // // this.afs.collection('users', ref => ref.where('userId', '==', 'Ayq6YA5pS1XXJtKfSLJNpvanzSj1')).valueChanges().subscribe(x => {
-      // //     console.log(x);
-      // // });
+      // });
 
+      //Read - list test
+
+      //Update
+      // this.afs.doc('/budgets/IHbuKGDQRFnM0YAm8znX').update(data);
 
   }
 
