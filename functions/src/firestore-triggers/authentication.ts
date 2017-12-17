c3fc40b295ad453c4a2e4a77394c3ff08c186245
Eric Larson <larsoneric@live.com>
2017-12-17 01:34:08 -0600

    Rebuild Firebase Functions

--- functions/src/firestore-triggers/authentication.ts
+++ functions/src/firestore-triggers/authentication.ts
@@ -5,11 +5,25 @@ import { User } from '../../../models/user.model';
 const db = admin.firestore();
 
 export const onUserCreate = functions.auth.user().onCreate(event => {
-
     const user: User = {
         userId: event.data.uid,
         email: event.data.email
     };
 
     return db.doc(`users/${user.userId}`).set(user, {merge: true});
+});
+
+
+export const onUserCreateAsync = functions.auth.user().onCreate(async event => {
+    const user: User = {
+        userId: event.data.uid,
+        email: event.data.email
+    };
+
+    try {
+        return await db.doc(`usersAsync/${user.userId}`).set(user, {merge: true});
+    } catch (error) {
+        console.error('Failed to added new user document');
+        throw error;
+    }
 });
--- functions/src/firestore-triggers/index.ts
+++ functions/src/firestore-triggers/index.ts
@@ -1,3 +1,6 @@
 import * as authentication from './authentication';
 
 export const onUserCreate = authentication.onUserCreate;
+export const onUserCreateAsync = authentication.onUserCreateAsync;
+
+
