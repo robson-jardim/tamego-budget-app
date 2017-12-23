75f5095347f619af557ca0d1dc39494f3c53385b
Eric Larson <larsoneric@live.com>
2017-12-23 02:21:07 -0600

    Added CORS Whitelist for Preflight Check

--- functions/src/express-api/index.ts
+++ functions/src/express-api/index.ts
@@ -4,9 +4,28 @@ import * as middleware from './middleware';
 import * as express from 'express';
 
 const app = express();
-app.use(cors({origin: true}));
-app.use(middleware.authenticate);
+
+const whitelist = [
+    'http://localhost:4200',
+    'https://budget-app-dev.firebaseapp.com/',
+    'https://budget-app-staging-1.firebaseapp.com/',
+    'https://budget-app-prod.firebaseapp.com/',
+    'https://app.tamego.com/'
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
+app.use(middleware.authenticate);
 
 // Controllers
 import { verifyUserController } from './controllers/verifyUserController';
