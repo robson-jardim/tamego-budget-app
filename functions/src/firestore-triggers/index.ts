99c9acab885327b2ff3839c897bdf819630f309c
Eric Larson <LarsonEric@live.com>
2018-01-31 17:07:03 -0600

    Removed Testing Suite

--- functions/src/firestore-triggers/index.ts
+++ functions/src/firestore-triggers/index.ts
@@ -1,5 +1,5 @@
 import * as authentication from './authentication';
-import * as categoryValues from './category-values/category-values';
+import * as categoryValues from './category-values';
 
 export const onUserCreate = authentication.onUserCreate;
 export const onValueCreate = categoryValues.onValueCreate;
