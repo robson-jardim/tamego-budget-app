82e24cab7733700871f764eb80f9012b163c703a
Eric Larson <LarsonEric@live.com>
2018-01-31 17:08:52 -0600

    Reduced Import For Functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin';
 admin.initializeApp(functions.config().firebase);
 
 import * as firestoreTriggers from './firestore-triggers/index';
-import { expressApi } from './express-api/index';
+import { expressApi } from './express-api';
 
 export const api = functions.https.onRequest(expressApi);
 
