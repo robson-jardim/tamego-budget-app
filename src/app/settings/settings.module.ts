1392ead61bf759a09a0264e027ca081817826397
Eric Larson <LarsonEric@live.com>
2018-02-01 00:50:41 -0600

    Removed Service Worker Console Log and Added Material Module to Settings Module

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings.component';
 import { HeaderModule } from '@shared/components/header/header.module';
 import { SettingsRoutingModule } from './settings-routing.module';
+import { StandardMaterialModule } from '@shared/components/standard-material.module';
 
 @NgModule({
     imports: [
         CommonModule,
         HeaderModule,
-        SettingsRoutingModule
+        SettingsRoutingModule,
+        StandardMaterialModule
     ],
     declarations: [
         SettingsComponent
