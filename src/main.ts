2a65d852ce49610819a5dfc4ce05e04e26afccf7
Eric Larson <LarsonEric@live.com>
2018-02-14 11:22:26 -0600

    Fixed Bug in Service Worker Updates

--- src/main.ts
+++ src/main.ts
@@ -10,21 +10,26 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-    // If the service work is registered in index it's going to immediately start trying
-    // to download all the assets that it needs to make this application work offline
-    // but that will happen while the app is loading and so we'll have two kind of
-    // different uses of the network competing with each other and will slow down the
-    // initial load
+// If the service work is registered in index it's going to immediately start trying
+// to download all the assets that it needs to make this application work offline
+// but that will happen while the app is loading and so we'll have two kind of
+// different uses of the network competing with each other and will slow down the
+// initial load
 
-    // Wait for angular to finish bootstrapping
-    // and then register the service worker
-    .then(() => registerServiceWorker())
+// Wait for angular to finish bootstrapping
+// and then register the service worker
+// .then(() => registerServiceWorker())
+// Service worker can not be registered here
+// because isUpdateAvailable promise needs to be set on window during registration
     .catch(err => console.log(err));
 
+registerServiceWorker();
+
+// Taken from: https://medium.com/progressive-web-apps/pwa-create-a-new-update-available-notification-using-service-workers-18be9168d717
 function registerServiceWorker() {
-    // Taken from: https://medium.com/progressive-web-apps/pwa-create-a-new-update-available-notification-using-service-workers-18be9168d717
     window['isUpdateAvailable'] = new Promise(function (resolve, reject) {
         if ('serviceWorker' in navigator) {
+
             navigator.serviceWorker.register('/service-worker.js').then(registration => {
                 console.log('[SW Registered]');
                 registration.onupdatefound = () => {
@@ -34,6 +39,7 @@ function registerServiceWorker() {
                             case 'installed':
                                 if (navigator.serviceWorker.controller) {
                                     // new update available
+                                    console.log('[SW] Updates available');
                                     resolve(true);
                                 } else {
                                     // no update available
