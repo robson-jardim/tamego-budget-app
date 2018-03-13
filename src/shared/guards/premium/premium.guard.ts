85231ecfe2a61f0392cf35ad63528fb14b846f95
Eric Larson <LarsonEric@live.com>
2018-03-13 02:52:07 -0500

    Simplified Premium Guard

--- src/shared/guards/premium/premium.guard.ts
+++ src/shared/guards/premium/premium.guard.ts
@@ -11,33 +11,12 @@ export class PremiumGuard implements CanActivate {
 
     canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
         return this.auth.user.first().map(user => {
-            if (user.isPremium) {
-
-                const isTrialExpired = this.isTrialExpired(user.trial);
-                const isTrial = user.trial.isTrial;
-
-                if (isTrial && isTrialExpired) {
-                    // This covers the case that the client has not been online since the trial ended
-                    // and therefore the user document does not have the updated isPremium value
-                    return false;
-                }
-                else {
-                    return true;
-                }
-            }
-            else {
-                return false;
-            }
+            return !!(user.isPremium);
         }).do(isPremium => {
             if (!isPremium) {
                 this.router.navigate(['settings'], {replaceUrl: true});
             }
         });
-    }
-
-    private isTrialExpired(trial) {
-        const now = new Date();
-        return now > trial.trialEnd;
     }
 }
 
