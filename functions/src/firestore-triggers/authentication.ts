506c61ae27a6ee50935e73d11427541752a5b296
Eric Larson <larsoneric@live.com>
2018-04-07 18:39:10 -0500

    Added Demo Budget on User Creation

--- functions/src/firestore-triggers/authentication.ts
+++ functions/src/firestore-triggers/authentication.ts
@@ -11,18 +11,23 @@ import { Subscription } from '../stripe/stripe-subscription';
 // Models
 import { User } from '@models/user.model';
 import { Customer } from '@models/customer.model';
+import { Budget } from '@models/budget.model';
+import { Account } from '@models/budget-account.model';
+
 import { EventContext } from 'firebase-functions/lib/cloud-functions';
+import { CategoryGroup } from '@models/category-group.model';
+import { Category } from '@models/category.model';
+
 
 export const onUserCreate = functions.auth.user()
     .onCreate(async (userRecord: UserRecord, context: EventContext) => {
 
-        const serverTimestamp: Date = admin.firestore.FieldValue.serverTimestamp() as any;
-
+        const nowTimestamp = new Date();
         const user: User = {
             userId: userRecord.uid,
             email: userRecord.email || null,
             emailVerified: false,
-            timeCreated: serverTimestamp,
+            timeCreated: nowTimestamp,
             customerId: null,
             subscriptionId: null,
             cardDetails: null,
@@ -53,6 +58,8 @@ export const onUserCreate = functions.auth.user()
         }
 
         try {
+            await createDemoBudget(user.userId);
+
             const batch = admin.firestore().batch();
 
             const userDocRef = db.doc(`users/${user.userId}`);
@@ -74,3 +81,107 @@ export const onUserCreate = functions.auth.user()
         }
 
     });
+
+async function createDemoBudget(userId: string) {
+
+    const promises: Array<Promise<any>> = [];
+
+    const nowTimestamp = new Date();
+
+    const budgetCollection = db.collection('budgets');
+    const demoBudget: Budget = {
+        userId: userId,
+        budgetName: 'Demo Budget',
+        currencyCode: 'USD',
+        timeCreated: nowTimestamp,
+        lastModified: nowTimestamp
+    };
+
+    const demoBudgetRef = await budgetCollection.add(demoBudget);
+    const demoBudgetId = demoBudgetRef.id;
+
+    const accountCollection = db.collection(`budgets/${demoBudgetId}/accounts`);
+    const demoCheckingAccount: Account = {
+        accountName: 'Checking',
+        position: 0
+    };
+
+    const demoSavingsAccount: Account = {
+        accountName: 'Savings',
+        position: 1
+    };
+
+    promises.push(accountCollection.add(demoCheckingAccount));
+    promises.push(accountCollection.add(demoSavingsAccount));
+
+    const categoryGroups = db.collection('budgets/' + demoBudgetId + '/categoryGroups');
+
+    const responsibilitiesGroup: CategoryGroup = {
+        groupName: 'Immediate Responsibilities',
+        position: 0
+    };
+    const responsibilitiesGroupRef = await categoryGroups.add(responsibilitiesGroup);
+    const responsibilitiesGroupId = responsibilitiesGroupRef.id;
+
+    const entertainmentGroup: CategoryGroup = {
+        groupName: 'Entertainment',
+        position: 1
+    };
+    const entertainmentGroupRef = await categoryGroups.add(entertainmentGroup);
+    const entertainmentGroupId = entertainmentGroupRef.id;
+
+    const categoriesCollection = db.collection(`budgets/${demoBudgetId}/categories`);
+
+
+    // Group 1 - Category 1
+    const incomeCategory: Category = {
+        groupId: responsibilitiesGroupId,
+        categoryName: 'Income',
+        position: 0
+    };
+    promises.push(categoriesCollection.add(incomeCategory));
+
+    // Group 1 - Category 2
+    const rentCategory: Category = {
+        groupId: responsibilitiesGroupId,
+        categoryName: 'Rent/Mortgage',
+        position: 1
+    };
+    promises.push(categoriesCollection.add(rentCategory));
+
+    // Group 1 - Category 3
+    const groceriesCategory: Category = {
+        groupId: responsibilitiesGroupId,
+        categoryName: 'Groceries',
+        position: 2
+    };
+    promises.push(categoriesCollection.add(groceriesCategory));
+
+
+    // Group 2 - Category 1
+    const eatingOutCategory: Category = {
+        groupId: entertainmentGroupId,
+        categoryName: 'Eating Out',
+        position: 0
+    };
+    promises.push(categoriesCollection.add(eatingOutCategory));
+
+    // Group 2 - Category 1
+    const musicCategory: Category = {
+        groupId: entertainmentGroupId,
+        categoryName: 'Music',
+        position: 1
+    };
+    promises.push(categoriesCollection.add(musicCategory));
+
+    // Group 2 - Category 2
+    const gamingCategory: Category = {
+        groupId: entertainmentGroupId,
+        categoryName: 'Gaming',
+        position: 2
+    };
+    promises.push(categoriesCollection.add(gamingCategory));
+
+
+    await Promise.all(promises);
+}
